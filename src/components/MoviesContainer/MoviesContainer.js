67e5672bfe43e39a3d348c0462ace3d753ca498c
Eric Meldrum <eric.meldrum@gmail.com>
2019-10-08 16:44:07 -0600

    Create MovieContainer and Movie components with initial data
    Co-authored-by: Amanda Consuelo Sierra <asierralopez526@gmail.com>

---
+++ src/components/MoviesContainer/MoviesContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './MoviesContainer.scss';
+import initialMovies from '../../initialData';
+import Movie from '../Movie/Movie'
+
+const MoviesContainer = (props) => {
+  const movieCards = () => {
+    return initialMovies.map(movie => {
+      return (<Movie 
+        key={movie.episode_id} 
+        id={movie.episode_id} 
+        title={movie.title} 
+        year={movie.release_date}
+        />
+      )
+    })
+  }
+
+  return (
+    <div>
+      {movieCards}
+    </div>  
+  )
+}
+
+export default MoviesContainer
---
+++ src/initialData.js
@@ -0,0 +1,46 @@
+const initialMovies = [
+  {
+  url: "https://swapi.co/api/films/1/",
+  episode_id: 4,
+  title: "A New Hope",
+  release_date: "1977"
+  },
+  {
+    url: "https://swapi.co/api/films/2/",
+    episode_id: 5,
+    title: "The Empire Strikes Back",
+    release_date: "1980"
+  },
+  {
+    url: "https://swapi.co/api/films/3/",
+    episode_id: 6,
+    title: "Return of the Jedi",
+    release_date: "1983"
+  },
+  {
+    url: "https://swapi.co/api/films/4/",
+    episode_id: 1,
+    title: "The Phantom Menace",
+    release_date: "1999"
+  },
+  {
+    url: "https://swapi.co/api/films/5/",
+    episode_id: 2,
+    title: "Attack of the Clones",
+    release_date: "2002"
+  },
+  {
+    url: "https://swapi.co/api/films/6/",
+    episode_id: 3,
+    title: "Revenge of the Sith",
+    release_date: "2005"
+  },
+  {
+    url: "https://swapi.co/api/films/7/",
+    episode_id: 7,
+    title: "The Force Awakens",
+    release_date: "2015"
+  }
+]
+
+export default initialMovies;
