e523684d85219fbc6eaae41e6dd06e86d5737c63
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-08 17:56:19 -0600

    Fetch characters and set them to a state
    Co-authored-by: Eric Meldrum <eric.meldrum@gmail.com>

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -3,3 +3,13 @@ export const getMovieData = (movieUrl) => {
      .then(response => response.json())
      .then(movieData => movieData)
 }
+
+export const getCharacters = (characterUrls) => {
+   const char = characterUrls.map((characterUrl) => {
+       return fetch(characterUrl)
+         .then(response => response.json())
+         .then(characterData => characterData)
+   })
+
+   return Promise.all(char);
+}
