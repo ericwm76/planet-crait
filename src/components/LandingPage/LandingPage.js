5b7d29ca2421a678687a016d3bea5737b47c6ce2
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-08 22:10:23 -0600

    Build up landing page component

---
+++ src/components/LandingPage/LandingPage.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import './LandingPage.scss';
+
+class LandingPage extends Component {
+    constructor() {
+      super();
+      this.state = {
+        name: '',
+        quote: ''
+      }
+    }
+
+    render() {
+        return(
+            <section className='landing-page'>
+              <form className='landing-form'>
+                  <input className='name-input' 
+                         type='text'
+                         placeholder='Name of Jedi'
+                         name='name'
+                         value={this.state.name} />
+                  <input className='quote-input' 
+                         type='text'
+                         placeholder='Your favourite Star Wars quote here'
+                         name='quote'
+                         value={this.state.title} />/>
+                  <div className='fandom-level-btns'>
+                    <button className='novice-btn'>Novice</button>
+                    <button className='intermediate-btn'>Intermediate</button>
+                    <button className='Expertnovice-btn'>Expert</button>
+                  </div>
+ 
+                  <button>Do. Or do not.</button>
+              </form>
+            </section>
+        )
+    }
+
+}
+
+export default LandingPage;
