77891bcb8900ad27a068141a47106478e6e822b8
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-08 22:51:59 -0600

    Finish basic design of landing page

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -13,26 +13,28 @@ class LandingPage extends Component {
     render() {
         return(
             <section className='landing-page'>
-                 <h2>Welcome to Planet Crait</h2>
-              <form className='landing-form'>
-                  <input className='name-input' 
+                <div className='form-section'>
+                  <h2>Welcome to Planet Crait</h2>
+                  <form className='landing-form'>
+                    <input className='name-input' 
                          type='text'
                          placeholder='Name of Jedi'
                          name='name'
                          value={this.state.name} />
-                  <input className='quote-input' 
+                    <input className='quote-input' 
                          type='text'
                          placeholder='Your favourite Star Wars quote here'
                          name='quote'
                          value={this.state.title} />
-                  <div className='fandom-level-btns'>
-                    <button className='novice-btn'>Novice</button>
-                    <button className='intermediate-btn'>Intermediate</button>
-                    <button className='Expertnovice-btn'>Expert</button>
-                  </div>
+                    <div className='fandom-level-btns'>
+                      <button className='novice-btn btn'>Novice</button>
+                      <button className='intermediate-btn btn'>Intermediate</button>
+                      <button className='expert-btn btn'>Expert</button>
+                    </div>
  
-                  <button className='submit-btn' >Do. Or do not. Submit</button>
-              </form>
+                    <button className='submit-btn' >Do. Or do not. Submit</button>
+                  </form>
+                </div>
             </section>
         )
     }
