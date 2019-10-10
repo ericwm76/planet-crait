31a4477321f7bb5559cf0dc6e7dc888f5559a58d
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-09 21:15:41 -0600

    Add font awesome cdn to html links

---
+++ src/components/Character/Character.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Character = (props) => {
+  return (
+      <section className='character'>
+        <h3>Name</h3>
+        <p>Homeworld</p>
+        <p>Population of Homeworld</p>
+        <p>Species</p>
+        <div className='related-films'>
+          <p>Related Films</p>
+
+        </div>
+        <div className='favourite-button'>
+
+        </div>
+      </section>
+  )
+}
+
+export default Character;
