413c49cbe629e4ba52effd188791e7c250a01507
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-09 21:06:30 -0600

    Add basic functional layout to chracters container

---
+++ src/components/CharactersContainer/CharactersContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Character from '../Character/Character';
+
+const CharactersContainer = (props) => {
+
+    return (
+      <section className='character-container'>
+        <Character />
+      </section>
+    )
+}
+
+export default CharactersContainer;
