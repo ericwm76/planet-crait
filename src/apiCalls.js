010586e2ea9d8678437d0795a64c2c448b4e3e25
Eric Meldrum <eric.meldrum@gmail.com>
2019-10-09 20:26:07 -0600

    Fetch data from species URL and return species name; return a completed Character object from the Promise.all

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -9,27 +9,34 @@ export const getMovieData = (movieUrl) => {
        return fetch(characterUrl)
          .then(response => response.json())
          .then(characterObj => {
-            const { name, homeworld, species, films } = characterObj;
+            const { films, homeworld, species } = characterObj;
             let promise1 = getFilms(films).then(filmTitle => filmTitle);
             let promise2 = getHomeworld(homeworld).then(worldObj => ({homeWorldName: worldObj.name, homeWorldPop: worldObj.population}));
-            return Promise.all([promise1, promise2]).then(data => console.log('data from promise.all--->', data))
+            let promise3 = getSpecies(species).then(speciesObj => speciesObj.name)
+            return Promise.all([promise1, promise2, promise3]).then(data => ({name: characterObj.name, species: data[2], homeworldName: data[1].homeWorldName, homeworldPop: data[1].homeWorldPop, appearsIn: data[0], favorited: false}))
          })
    })
 
    return Promise.all(char);
 }
 
-const getHomeworld = (worldUrl) => {
+const getHomeworld = worldUrl => {
    return fetch(worldUrl)
    .then(response => response.json())
    .then(homeWorld => ({ name: homeWorld.name, population: homeWorld.population }));
 }
 
-const getFilms = (filmUrls) => {
+const getFilms = filmUrls => {
    const films = filmUrls.map(filmUrl => {
       return fetch(filmUrl)
       .then(response => response.json())
       .then(filmObj => filmObj.title)
    })
    return Promise.all(films);
+}
+
+const getSpecies = species => {
+   return fetch(species[0])
+   .then(response => response.json())
+   .then(species => species)
 }
