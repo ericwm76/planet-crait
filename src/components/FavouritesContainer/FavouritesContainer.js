9013b49673be4327c5139d3df752d11130adb6a4
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-09 21:21:20 -0600

    Add basic outline to favourites container

---
+++ src/components/FavouritesContainer/FavouritesContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Favourite from '../Favourite/Favourite';
+
+const FavouritesContainer = (props) => {
+
+    return (
+      <section className='Favourites-container'>
+        <Favourite />
+      </section>
+    )
+}
+
+export default FavouritesContainer;
