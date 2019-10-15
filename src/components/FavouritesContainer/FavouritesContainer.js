5f89318c6943b6e3fe9226dd3261e5893a36a256
Eric Meldrum <eric.meldrum@gmail.com>
2019-10-15 16:30:47 -0600

    Conditionally render favorites with an error msg if there are none

--- src/components/FavouritesContainer/FavouritesContainer.js
+++ src/components/FavouritesContainer/FavouritesContainer.js
@@ -5,19 +5,27 @@ import SideBar from '../SideBar/SideBar';
 import Character from '../Character/Character';
 
 const FavouritesContainer = (props) => {
-  const characterCards = props.favourites.map((character, i) => {
-    return (
-      <Character
-        character={character}
-        updateFavourites={props.updateFavourites}
-        key={ i + character.name }
-      /> 
-    )
-  })
+  const noFavsMsg = () => {
+    if (props.favourites.length) {
+      return props.favourites.map((character, i) => {
+        return (
+          <Character
+          character={character}
+          updateFavourites={props.updateFavourites}
+          key={ i + character.name }
+          /> 
+        )
+      })
+    } else {
+      return (
+      <p className="no-fav-msg">Add some favourites!</p>
+      )
+    }
+  }
     return (
       <section className='Favourites-container'>
         <SideBar />
-        {characterCards}
+        {noFavsMsg()}
       </section> 
     ) 
 }
