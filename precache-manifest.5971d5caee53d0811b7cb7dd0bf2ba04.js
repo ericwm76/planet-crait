7c4110d760296800f19ff70ee909cf791bfc71b0
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2019-10-15 17:02:42 -0600

    Updates

---
+++ precache-manifest.5971d5caee53d0811b7cb7dd0bf2ba04.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7453f2d7a8c5d434c99b4375fb90ac12",
+    "url": "/planet-crait/index.html"
+  },
+  {
+    "revision": "6eb9a484746e0c49bc7e",
+    "url": "/planet-crait/static/css/main.2b43b46c.chunk.css"
+  },
+  {
+    "revision": "0f93bc1f3ae02b7ae964",
+    "url": "/planet-crait/static/js/2.85ef50a4.chunk.js"
+  },
+  {
+    "revision": "6eb9a484746e0c49bc7e",
+    "url": "/planet-crait/static/js/main.435c33b2.chunk.js"
+  },
+  {
+    "revision": "36380ef22234d2f6849a",
+    "url": "/planet-crait/static/js/runtime-main.3b87b551.js"
+  }
+]);
